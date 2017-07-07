2414d7f117e8d9008c65d5cc9eb9c2ff0b3f04fd
TWQ1765 <www.taaadaaa@qq.com>
2017-07-07 19:50:05 +0800

    trying sprintf

--- test/test_intel_hex.c
+++ test/test_intel_hex.c
@@ -8,23 +8,45 @@ void setUp(void)
 void tearDown(void)
 {
 }
-
+//test1: file TestCode occur or not?
 void test_intel_hex_fopen(void)
 {
     FILE *file;
     file = fopen("data/TestCode.hex","r");
     TEST_ASSERT_NOT_NULL(file);
 }
-
+//test2: file Blinky.X.production occur or not?
 void test_intel_hex2_fopen(void)
 {
     FILE *file;
     file = fopen("doc/Blinky.X.production.hex","r");
     TEST_ASSERT_NOT_NULL(file);
 }
+/*//trying scanf ()
 void test_scanf(void)
 {
   int x;
   scanf("%d" , &x);
   printf("This is a test value must be int : %d\n" , x);
 }
+*/
+///*//trying sscanf()
+void test_sscanf(void)
+{
+  char iHexLine[] = "060023002c45ab38949c45";
+  int value;
+  
+  sscanf(&iHexLine[0] , "%2x" , &value);
+  TEST_ASSERT_EQUAL_HEX32(0xabcdef, value);
+}
+void test_sprintf_()
+{
+  char *buffer;
+  
+  buffer = malloc(1028);
+  sprintf(buffer, "Hello Word 0x%)
+}
+//*/
+/*//trying fgets()
+void test_intel_hex2_fopen(void)
+*/
